e2953009cdf0661e60fbca1b9901ba40e60e9c0d
Varad Pensalwar <varadpensalwar@gmail.com>
2025-04-06 18:21:43 +0530

    Video's Added

--- src/videoData.js
+++ src/videoData.js
@@ -1,23 +1,30 @@
 // src/videoData.js
 const videoData = [
-    { src: 'videos/Porsche_911_GT3_RS_12.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_1.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_2.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_3.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_4.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_5.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_6.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_7.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_16.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_9.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_10.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_11.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_13.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_14.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_15.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_8.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_17.mp4', model: 'Porsche 911 GT3 RS' },
-    { src: 'videos/Porsche_911_GT3_RS_18.mp4', model: 'Porsche 911 GT3 RS' }
+  { src: 'videos/Porsche_911_GT3_RS_12.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_1.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_19.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_2.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_20.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_3.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_21.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_4.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_22.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_5.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_23.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_6.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_24.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_7.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_16.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_9.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_10.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_11.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_13.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_14.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_15.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_8.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_17.mp4', model: 'Porsche 911 GT3 RS' },
+  { src: 'videos/Porsche_911_GT3_RS_18.mp4', model: 'Porsche 911 GT3 RS' }
+
   ];
   
   export default videoData;
