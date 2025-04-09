d30c92af1d1fd71d9433044c9205999cc76a21e6
Varad Pensalwar <varadpensalwar@gmail.com>
2025-04-09 21:17:11 +0530

    Project Completed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './App.css';  // Adjust the path if your file is named differently or in a subdirectory
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
